e84c37ac9ad60c1fdf42a96ec44dafc7d6d9c369
kg29185048 <gargkrish152.com>
2025-10-03 17:42:50 +0530

    push database detaails

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constant";
 
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
 
+//ifee
+//database connection
+//always use async await
+(
+  async ()=>{
+    try{
+      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+      
+      app.on("error",(error)=>{
+          console.log("ERROR: ",error);
+          throw error;  
+      })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+      app.listen(process.env.PORT,()=>{
+          console.log(`app is listening on port ${process.env.PORT}`);
+      })
+
+    }
+    catch(error){
+      console.log("ERROR: ",error);
+      throw error;
+    }
+  }
+)()
